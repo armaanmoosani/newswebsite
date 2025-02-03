e3f58071a499aaed01c9fa682f0ec4d981372630
armaanmoosani <107946049+armaanmoosani@users.noreply.github.com>
2025-02-03 11:21:00 -0600

    cleanup

--- module3/database.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-    //from wiki
-    $mysqli = new mysqli('localhost', 'phpuser', 'a8ushd742%o43(2jx@*ujsui', 'module3');
-    if($mysqli->connect_errno) {
-        printf("Connection Failed: %s\n", $mysqli->connect_error);
-        exit;
-    }
-?>
